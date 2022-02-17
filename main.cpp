d3724be8080987acef8cb057329a32f71a9e7f9d
Yaroslav Karpov <yaroslav.karpov.2016@mail.ru>
2022-02-17 21:09:12 +0400

    still not working

--- main.cpp
+++ main.cpp
@@ -1,30 +1,23 @@
-#include <QMainWindow>
-#include <QtGui>
-#include <QApplication>
-#include <QGridLayout>
-#include <QDir>
-#include <QLabel>
+#include "main.h"
 
 #include <iostream>
-
-#include "main.h"
 
 #define DEFAULT_WIDTH 800
 #define DEFAULT_HEIGHT 600
 #define PATH_CATHEGORIES "/usr/share/alterator/desktop-directories/"
 #define PATH_MODULES "/usr/share/alterator/applications/"
 
-class AppButton: public QPushButton {
+class appbt: public QPushButton {
 	public:
-		/*
-		AppButton(QString *path_to_desktop) {
-			for (QFileInfoList fileName : res){
-			}
+		appbt (QString Name, QString Icon, QString Cath) {
+			appName = Name;
+			appIcon = Icon;
+			appCath = Cath;
 		}
-		*/
-		void setName(QString a){appName = a;}
-		void setIcon(QString a){appIcon = a;}
-		void setCath(QString a){appCath = a;}
+
+		void setName(QString val){appName = val;}
+		void setIcon(QString val){appIcon = val;}
+		void setCath(QString val){appCath = val;}
 
 		QString getName(){return appName;}
 		QString getIcon(){return appIcon;}
@@ -37,13 +30,16 @@ class AppButton: public QPushButton {
 };
 
 
-
 int main(int argc, char *argv[]) {
 
 	// объявляем элементы
 	QApplication app(argc, argv);
 	QMainWindow w;
 	QGridLayout* grid = new QGridLayout;
+	QTextStream os(stdout);
+
+	QList<appbt>   app_buttons;
+	QList<QString> cathegories;
 
 	// получаем список приложений
 	QDir dir;
@@ -51,45 +47,46 @@ int main(int argc, char *argv[]) {
 	dir.setFilter(QDir::Files);
 	QFileInfoList app_desktop_files = dir.entryInfoList();
 
-	QList<AppButton>* app_buttons;
-	QList<QString> cathedories;
 
 	// создаем массив кнопок QPushButton
 	// забиваем их в QList 
 	for (QFileInfo fileName : app_desktop_files) {
-		AppButton *button = new AppButton;
 
 		QSettings settings(fileName.absoluteFilePath(), QSettings::IniFormat);
 				settings.beginGroup("Desktop Entry");
-				button->setName (settings.value("Name").toString());
-				button->setIcon (settings.value("Icon").toString());
-				button->setCath (settings.value("Categories").toString());
+				QString Name = settings.value("Name").toString();
+				QString Icon = settings.value("Icon").toString();
+				QString Cath = settings.value("Categories").toString();
 		settings.endGroup();
 
-		cathedories << button->getCath();
-		app_buttons->insert(button->getName, *button);
-
+		appbt *button = new appbt(Name, Icon, Cath);
+		cathegories << button->getCath();
+		os << button->getCath();
+		// os << button->getName()
+		// app_buttons->insert(button->getName(), *button);
 	}
 
 	// заливаем в grid копки
+	/*
 	int row=0;
-	for (QString cath : cathedories) {
+	for (QString cath : cathegories) {
 		int col=0;
 
 		QLabel *lcath = new QLabel;
 		lcath->setText(cath);
 		grid->addItem(*lcath, row++, col, 1, 1);
 
-		for (AppButton *btn : app_buttons){
-			if (btn->getCath==cath) {
+		for (appbt *btn : app_buttons){
+			if ( btn->getCath == cath) {
 				grid->addItem(btn, row, col++);
 			}
 		}
 	}
-
+*/
 
 	grid->setMargin(5);
 	grid->setSpacing(15);
+
 	w.resize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
 	w.setLayout(grid);
 	w.show();
--- main.h
+++ main.h
@@ -1,2 +1,14 @@
-#include <QString>
+// widgets
+#include <QMainWindow>
+#include <QApplication>
+#include <QGridLayout>
+#include <QPushButton>
 
+// fisesystem
+#include <QDir>
+
+// string
+#include <QString>
+#include <QLabel>
+#include <QSettings>
+#include <QTextStream>
